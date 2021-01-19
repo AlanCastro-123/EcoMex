5002fe666620765eddbf0b070e0d8c63fbe1f639
AlanCastro-123 <confi-1998@hotmail.com>
2021-01-18 18:42:04 -0600

    Base inicial del proyecto

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Eco-Mex</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/nuestros_estilos.css">
+    
+  </head>
+  <body>
+    <?php include 'templates/header.php';?>
+    <br>
+    <?php 
+      session_start();
+    ?>
+    <br>
+    <?php include 'templates/footer.php';?>
+  </body>
+</html>
